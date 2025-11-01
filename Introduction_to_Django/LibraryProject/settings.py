e9145a02165737590b658356f742d7541e64b0ba
Fikerte Yimer <fikerteyimer06gmail.com>
2025-11-01 21:23:14 +0300

    Fix manage.py and settings.py for TIFFS submission

---
+++ Introduction_to_Django/LibraryProject/settings.py
@@ -0,0 +1,19 @@
+"""
+Django settings for LibraryProject project.
+
+This is a minimal placeholder file for submission purposes.
+"""
+
+SECRET_KEY = 'django-insecure-placeholder'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = []
+
+MIDDLEWARE = []
+
+ROOT_URLCONF = 'LibraryProject.urls'
+
+TEMPLATES = []
+
+WSGI_APPLICATION = 'LibraryProject.wsgi.application'
