b2c23d06ad1928127f2a0cd1a8178e2aa085bdd8
Fikerte Yimer <fikerteyimer06gmail.com>
2025-11-30 16:37:46 +0300

    update test_view.py

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -5,21 +5,27 @@ from .models import Book, Author
 from django.contrib.auth.models import User
 
 class BookTests(APITestCase):
-    def setUp(self):
-        # Create test data
-        self.user = User.objects.create_user(
-            username='testuser', 
-            password='testpass123'
-        )
-        self.author = Author.objects.create(name='Test Author')
-        self.book = Book.objects.create(
-            title='Test Book',
-            publication_year=2020,
-            author=self.author
-        )
+def setUp(self):
+# Create test user
+self.user = User.objects.create_user(
+username='testuser',
+password='testpass123'
+)
+# Log in the test client
+self.client.login(username='testuser', password='testpass123')
 
-    def test_list_books(self):
-        url = reverse('book-list')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+```
+    # Create test data
+    self.author = Author.objects.create(name='Test Author')
+    self.book = Book.objects.create(
+        title='Test Book',
+        publication_year=2020,
+        author=self.author
+    )
+
+def test_list_books(self):
+    url = reverse('book-list')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(len(response.data), 1)
+```
